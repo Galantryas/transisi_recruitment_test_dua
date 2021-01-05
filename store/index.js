9604835473c9014eccf42c00cddbddf7021f7f54
Galantryas Mukti Pratama <galantryas.2014@gmail.com>
2021-01-05 22:52:00 +0700

    add vuex for auth token

---
+++ store/index.js
@@ -0,0 +1,19 @@
+export const types = {
+  };
+  
+  export const state = () => ({
+      token: null,
+  });
+  
+  export const getters = {
+  };
+  
+  export const mutations = {
+    changeToken(state, newToken) {
+      state.token = newToken;
+    },
+  };
+  
+  export const actions = {
+  };
+  
